f76889042f8dd9b383e712efc1a397a53a1add22
gebre17 <yehubel23@gmail.com>
2023-03-17 13:30:00 +0300

    feat: add Vehicle model and CRUD service layer

---
+++ backend/app/models/vehicle.py
@@ -0,0 +1,59 @@
+"""Vehicle model."""
+from uuid import uuid4
+from datetime import datetime, timezone
+from sqlalchemy import Column, String, Integer, DateTime, Enum, ForeignKey, Index
+from sqlalchemy.dialects.postgresql import UUID
+from app.core.database import Base
+import enum
+
+
+class VehicleType(str, enum.Enum):
+    """Vehicle types."""
+    CAR = "car"
+    TRUCK = "truck"
+    MOTORCYCLE = "motorcycle"
+    VAN = "van"
+
+
+class VehicleStatus(str, enum.Enum):
+    """Vehicle status."""
+    ACTIVE = "active"
+    IDLE = "idle"
+    OFFLINE = "offline"
+    MAINTENANCE = "maintenance"
+
+
+class Vehicle(Base):
+    """Vehicle model."""
+    __tablename__ = "vehicles"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    name = Column(String(100), nullable=False)
+    plate_number = Column(String(20), unique=True, nullable=False, index=True)
+    type = Column(Enum(VehicleType), nullable=False)
+    make = Column(String(50))
+    model = Column(String(50))
+    year = Column(Integer)
+    device_id = Column(String(100), unique=True, index=True)
+    status = Column(
+        Enum(VehicleStatus),
+        default=VehicleStatus.OFFLINE,
+        nullable=False,
+    )
+    color = Column(String(30))
+    created_at = Column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
+    updated_at = Column(
+        DateTime(timezone=True),
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+        nullable=False,
+    )
+
+    __table_args__ = (
+        Index("ix_vehicles_user_id", "user_id"),
+    )
---
+++ backend/app/services/vehicle_service.py
@@ -0,0 +1,177 @@
+"""Vehicle service."""
+from typing import Optional, List
+from uuid import UUID
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, func
+from app.models.vehicle import Vehicle, VehicleStatus
+from app.schemas.vehicle import VehicleCreate, VehicleUpdate
+from fastapi import HTTPException, status
+
+
+class VehicleService:
+    """Service for vehicle operations."""
+    
+    @staticmethod
+    async def create_vehicle(
+        user_id: UUID,
+        vehicle_data: VehicleCreate,
+        db: AsyncSession,
+    ) -> Vehicle:
+        """
+        Create a new vehicle.
+        
+        Args:
+            user_id: Owner user ID
+            vehicle_data: Vehicle creation data
+            db: Database session
+        
+        Returns:
+            Created vehicle
+        """
+        vehicle = Vehicle(
+            user_id=user_id,
+            **vehicle_data.model_dump()
+        )
+        
+        db.add(vehicle)
+        await db.commit()
+        await db.refresh(vehicle)
+        
+        return vehicle
+    
+    @staticmethod
+    async def get_vehicle(
+        vehicle_id: UUID,
+        user_id: UUID,
+        db: AsyncSession,
+    ) -> Vehicle:
+        """
+        Get vehicle by ID.
+        
+        Args:
+            vehicle_id: Vehicle ID
+            user_id: User ID (for authorization)
+            db: Database session
+        
+        Returns:
+            Vehicle
+        
+        Raises:
+            HTTPException: If vehicle not found or unauthorized
+        """
+        stmt = select(Vehicle).where(
+            (Vehicle.id == vehicle_id) & (Vehicle.user_id == user_id)
+        )
+        result = await db.execute(stmt)
+        vehicle = result.scalar_one_or_none()
+        
+        if not vehicle:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Vehicle not found",
+            )
+        
+        return vehicle
+    
+    @staticmethod
+    async def list_vehicles(
+        user_id: UUID,
+        skip: int = 0,
+        limit: int = 50,
+        db: AsyncSession = None,
+    ) -> tuple[List[Vehicle], int]:
+        """
+        List vehicles for a user.
+        
+        Args:
+            user_id: User ID
+            skip: Number of records to skip
+            limit: Number of records to return
+            db: Database session
+        
+        Returns:
+            Tuple of (vehicles, total_count)
+        """
+        stmt = select(Vehicle).where(Vehicle.user_id == user_id)
+        
+        # Get total count
+        count_stmt = select(func.count()).select_from(Vehicle).where(Vehicle.user_id == user_id)
+        count_result = await db.execute(count_stmt)
+        total = count_result.scalar()
+        
+        # Get paginated results
+        stmt = stmt.offset(skip).limit(limit)
+        result = await db.execute(stmt)
+        vehicles = result.scalars().all()
+        
+        return vehicles, total
+    
+    @staticmethod
+    async def update_vehicle(
+        vehicle_id: UUID,
+        user_id: UUID,
+        vehicle_data: VehicleUpdate,
+        db: AsyncSession,
+    ) -> Vehicle:
+        """
+        Update a vehicle.
+        
+        Args:
+            vehicle_id: Vehicle ID
+            user_id: User ID (for authorization)
+            vehicle_data: Update data
+            db: Database session
+        
+        Returns:
+            Updated vehicle
+        """
+        vehicle = await VehicleService.get_vehicle(vehicle_id, user_id, db)
+        
+        update_data = vehicle_data.model_dump(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(vehicle, key, value)
+        
+        await db.commit()
+        await db.refresh(vehicle)
+        
+        return vehicle
+    
+    @staticmethod
+    async def delete_vehicle(
+        vehicle_id: UUID,
+        user_id: UUID,
+        db: AsyncSession,
+    ) -> None:
+        """
+        Delete (soft delete) a vehicle.
+        
+        Args:
+            vehicle_id: Vehicle ID
+            user_id: User ID (for authorization)
+            db: Database session
+        """
+        vehicle = await VehicleService.get_vehicle(vehicle_id, user_id, db)
+        
+        # Soft delete by marking as offline (can be extended with a deleted flag)
+        vehicle.status = VehicleStatus.OFFLINE
+        
+        await db.commit()
+    
+    @staticmethod
+    async def get_vehicle_by_device_id(
+        device_id: str,
+        db: AsyncSession,
+    ) -> Optional[Vehicle]:
+        """
+        Get vehicle by device ID.
+        
+        Args:
+            device_id: IoT device ID
+            db: Database session
+        
+        Returns:
+            Vehicle or None
+        """
+        stmt = select(Vehicle).where(Vehicle.device_id == device_id)
+        result = await db.execute(stmt)
+        return result.scalar_one_or_none()
