de8bea12a475ff09a49dd84dcb827b00f0860f15
gebre17 <yehubel23@gmail.com>
2023-04-01 10:00:00 +0300

    chore: initialize Next.js frontend with TypeScript and Tailwind

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0066cc",
+        secondary: "#666666",
+      },
+    },
+  },
+  plugins: [],
+  darkMode: "class",
+}
